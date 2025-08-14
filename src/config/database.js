6641b7de102fbdec1f7b0bd2ef348aa572d92812
Matias Silvera <matusilvera808@gmail.com>
2025-08-14 10:06:39 -0300

    Se configuro la conexion con la db

---
+++ src/config/database.js
@@ -0,0 +1,23 @@
+import { Sequelize } from "sequelize";
+import "dotenv/config"
+
+export const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+    }
+)
+
+export const initDB = async ()=>{
+    try {
+        await sequelize.authenticate()
+        console.log("Se autenticó la conexión con la DB");
+        await sequelize.sync({force: true})
+        console.log("Se sincronizó con la DB");
+    } catch (err) {
+        console.log(`Error al conectarde con la DB: ${err}`);
+    }
+}
